15d719c52896e75856a2da3cfbc492a034ec0f18
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-09-21 20:39:17 -0300

    feat: Criado a funcionalidade de criar registro

--- js/create-register-service.js
+++ js/create-register-service.js
@@ -1,6 +1,21 @@
-export const createRegisterService = (register) => {
-  await fetch(`${BASE_URL}`, { method: 'POST', body: register })
+import { BASE_URL } from '../contants/endpoint.constant.js';
 
+const form = document.getElementById('form-create');
+
+export const createRegisterService = async (event) => {
+  event.preventDefault();
+
+  const { nome,genero, status, email } = event.target;
+
+  const formData = new FormData();
+  formData.set('name', nome.value);
+  formData.set('gender', genero.value);
+  formData.set('status', status.value);
+  formData.set('email', email.value);
+
+  await fetch(`${BASE_URL}`, { method: 'POST', body: formData })
 
   window.location.replace('/');
 }
+
+form.addEventListener('submit', createRegisterService);
